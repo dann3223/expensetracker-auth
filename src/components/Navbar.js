fcfc85f6a3d36c978a22cc4fd93a01ab58020568
dann3223 <dante0im@gmail.com>
2022-04-25 09:44:11 -0300

    login & signup fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import { NavLink } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
+import React from "react";
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
+	const { logout } = useLogout();
+	const { user } = useAuthContext();
 
-  return (
-    <nav className={styles.navbar}>
-      <ul>
-        <li className={styles.title}>MyTracker</li>
+	return (
+		<nav className={styles.navbar}>
+			<ul>
+				<li className={styles.title}>MyTracker</li>
 
-        {!user && (
-          <>
-            <li><NavLink to='/login'>Login</NavLink></li>
-            <li><NavLink to='/signup'>Sign Up</NavLink></li>
-          </>
-        )}
+				{!user && (
+					<>
+						<li>
+							<NavLink to="/login">Login</NavLink>
+						</li>
+						<li>
+							<NavLink to="/signup">Sign Up</NavLink>
+						</li>
+					</>
+				)}
 
-        {user && (
-          <>
-            <li className={styles.greeting}>Hello, {user.displayName}</li>
-            <li><button className="btn" onClick={logout}>Logout</button></li>
-          </>
-        )}
-      </ul>
-    </nav>
-  )
-}
+				{user && (
+					<>
+						<li className={styles.greeting}>Hello, {user.displayName}</li>
+						<li>
+							<button className={styles.specialButton} onClick={logout}>
+								Logout
+							</button>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,41 +1,47 @@
-import React, { useState } from 'react'
-import styles from './Login.module.css'
-import { useLogin } from '../../hooks/useLogin'
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const { login, error, isPending } = useLogin()
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { login, error, isPending } = useLogin();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    login(email, password)
-  }
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (email && password) {
+			login(email, password);
+		}
+	};
 
-  return (
-    <form onSubmit={handleSubmit} className={styles['login-form']}>
-      <h2>Login</h2>
-      <label>
-        <span>email:</span>
-        <input 
-          type="text" 
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input 
-          type="password" 
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      {!isPending && <button className='btn'>Login</button>}
-      {isPending && <button className='btn' disabled>Loading</button> }
-      {error && <p>{error}</p>}
-    </form>
-  )
-}
+	return (
+		<form onSubmit={handleSubmit} className={styles["login-form"]}>
+			<h2>Login</h2>
+			<label>
+				<span>Email:</span>
+				<input
+					type="text"
+					onChange={e => setEmail(e.target.value)}
+					value={email}
+				/>
+			</label>
+			<label>
+				<span>Password:</span>
+				<input
+					type="password"
+					onChange={e => setPassword(e.target.value)}
+					value={password}
+				/>
+			</label>
+			{!isPending && <button className="btn">Login</button>}
+			{isPending && (
+				<button className="btn" disabled>
+					Loading
+				</button>
+			)}
+			{error && <p>{error}</p>}
+		</form>
+	);
+};
 
-export default Login
+export default Login;
